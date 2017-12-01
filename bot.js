bec5ed99c3516262681bb8f8a1bc403cc5660764
geertsoetens <geert.soetens@ictmbo.nl>
2017-12-01 16:46:24 +0100

    Added allot of stuff to bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.io');
 const logger = require('winston');
 const auth = require('./auth.json');
+const info = require('./package.json');
 
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -31,11 +32,6 @@ bot.on('ready', function (evt) {
     });
 });
 
-
-/*
-Bot checking for messages that s
- */
-
 bot.on('message', function (user, userID, channelID, message, evt) {
 
     if (message.substring(0, 1) === '!') {
@@ -46,6 +42,34 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         args = args.splice(1);
         switch (cmd) {
 
+            case 'd':
+                if (!isNaN(args[0])) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "Rolling a custom d" + args[0]
+                    });
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "The dice has rolled! Result: " + getRandomNumber(1, (args[0] + 1)),
+                        typing: true
+                    })
+                } else if (typeof args[1] !== 'undefined') {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "I only need one argument: `!d [number]`."
+                    })
+                } else if (typeof args[0] === 'undefined') {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "I need a max number: `!d [number]`."
+                    })
+                } else {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "Erm, I can only roll numbers I'm afraid: `!d [number]`."
+                    });
+                }
+                break;
             case 'ping':
                 switch (getRandomNumber(1, 11)) {
 
@@ -176,6 +200,10 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: 'Stefan is de coolste persoon op de wereld.',
                     typing: true
                 });
+                bot.sendMessage({
+                    to: 256131905462730752,
+                    message: user + ' heeft !stefan gebruikt. Nog iemand weet dus dat jij cool bent.'
+                });
                 break;
             case 'niko':
                 bot.sendMessage({
@@ -185,6 +213,69 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.uploadFile({
                     to: channelID,
                     file: "images/1484600049_niko.gif"
+                });
+                break;
+            // Private messaging is broken in my version of DISCORD.IO. You have to fix it by changing:
+                /*
+                    DCP.createDMChannel = function(userID, callback) {
+                        var client = this;
+                        this._req('post ', Endpoints.USER(client.id) + "/channels", {recipient_id: userID}, function(err, res) {
+                            if (!err && goodResponse(res)) client._uIDToDM[res.body.recipient_id] = res.body.id;
+                            handleResCB("Unable to create DM Channel", err, res, callback);
+                        });
+                    };
+
+                    recipient_id && recipient.id should both be recipient_id!!!
+
+                    on Index.js line 923
+                */
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Check your private messages, <@!" + userID + ">!"
+                });
+                bot.sendMessage({
+                    to: userID,
+                    message: '```\n' +
+                    'Here\'s a list of all currently existing commands. \n' +
+                    'Any suggestions? Please send them to info@geertsoetens.nl \n' +
+                    '\n' +
+                    '!ping              -   Ping the bot, he\'ll reply with pong.\n' +
+                    '!d20               -   Generate a number 1-20.\n' +
+                    '!d6                -   Generate a number 1-6.\n' +
+                    '!d4                -   Generate a number 1-4.\n' +
+                    '!d8                -   Generate a number 1-8.\n' +
+                    '!d10               -   Generate a number 1-10.\n' +
+                    '!d12               -   Generate a number 1-12.\n' +
+                    '!roll              -   Roll a random number between 1-100\n' +
+                    '\n' +
+                    'To roll a custom dice:\n' +
+                    '!d [number]        -   Roll a dice, number = max.\n' +
+                    '\n' +
+                    '!truth             -   Function added for a friend.\n' +
+                    '!meaningoflife     -   Function added for a friend, replies with the meaning of life.\n' +
+                    '!stefan            -   Function added for a friend.\n' +
+                    '!niko              -   You gotta try this one out.\n' +
+                    '!info              -   Misc info about WARLOCK & contact info' +
+                    '```'
+                });
+                break;
+            case 'info':
+                bot.sendMessage({
+                   to: channelID,
+                   message: "Info has been sent to you privately! <@!" + userID + "> !"
+                });
+                bot.sendMessage({
+                    to: userID,
+                    message: "```\n" +
+                    "WARLOCK ver: " + info.version + "\n" +
+                    "Developed by: " + info.author + "\n" +
+                    "Description: " + info.description + "\n" +
+                    "For more info, please visit: http://geertsoetens.nl.\n" +
+                    "For any questions & suggestions, please e-mail:\n" +
+                    "       info@geertsoetens.nl\n" +
+                    "I hope you enjoy warlock bot, feel free to contact me!\n" +
+                    "```"
                 });
                 break;
             default:
@@ -242,7 +333,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             }
         }
 
-        logger.info(bot.username + ' (' + bot.id + ') ' + 'Replied to a message successfully.');
+        logger.info(bot.username + ' - (' + bot.id + ') ' + 'Replied to a message successfully.');
     }
 
     if (message.match(/daan/i)) {
@@ -274,5 +365,27 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             to: channelID,
             message: 'NANI!?!?!?!?!?!'
         });
+    }
+
+    if (message.match(/fuck\s*you\s*warlock/i)) {
+        bot.sendMessage({
+            to: channelID,
+            message: 'Woa bro, no need to get violent...'
+        });
+        if (userID !== "197416203441012739") {
+            bot.sendMessage({
+                to: userID,
+                message: 'Alright listen here you little shit, you dare insult me? I\'m motherfucking WARLOCK! I summon fucking demons for a living. And you think you got a chance? Kid... Please.'
+            });
+        } else {
+            bot.sendMessage({
+                to: userID,
+                message: 'Mawstew, pwease down\'t huwt my feewings :\'('
+            })
+        }
+        logger.warn(bot.username + " - (" + bot.id + ") " + "Has just been insulted, and has insulted the user back.");
+        logger.warn("// START ORIGINAL MESSAGE //");
+        logger.warn(user + ": " + message);
+        logger.warn("// END ORIGINAL MESSAGE //");
     }
 });
